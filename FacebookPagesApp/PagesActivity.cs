7d882a87b0ee475957c9e2e6edb986e8deb0e8f7
Dave <bordoley@gmail.com>
2015-02-14 14:01:23 -0800

    More work on the PagesView. Add the new post button

--- FacebookPagesApp/PagesActivity.cs
+++ FacebookPagesApp/PagesActivity.cs
@@ -5,16 +5,23 @@ using Android.Content;
 using Android.OS;
 using Android.Views;
 using Android.Widget;
+using Android.Support.V4.Widget;
 
 using ReactiveUI;
 using RxApp;
 
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
 
 namespace FacebookPagesApp
 {
     [Activity(Theme = "@android:style/Theme.Holo.Light")]    
     public class PagesActivity : RxActivity<IPagesViewModel>
     {
+
+        private SwipeRefreshLayout refresher;
+        private IDisposable subscription = null;
+
         public PagesActivity()
         {
         }
@@ -23,15 +30,47 @@ namespace FacebookPagesApp
         {
             base.OnCreate(bundle);
             this.SetContentView(Resource.Layout.Pages);
+
+            refresher = FindViewById<SwipeRefreshLayout>(Resource.Id.refresher);
         }
 
         protected override void OnResume()
         {
             base.OnResume();
+
+            var subscription = new CompositeDisposable();
+
+            subscription.Add(
+                Observable.FromEventPattern(refresher, "Refresh").Subscribe(_ => 
+                    this.ViewModel.RefeshPosts.Execute(null)));
+
+            subscription.Add(
+                this.WhenAnyValue(x => x.ViewModel.RefreshingPosts).Where(x => !x).Subscribe(_ => 
+                    refresher.Refreshing = false));
+                
+            this.subscription = subscription;
+        }
+
+        public override bool OnCreateOptionsMenu (IMenu menu)
+        {
+            MenuInflater.Inflate (Resource.Menu.PagesActionBarMenu, menu);       
+            return base.OnCreateOptionsMenu(menu);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.pages_action_bar_new_post:
+                    this.ViewModel.CreatePost.Execute(null);
+                    break;
+            }
+            return base.OnOptionsItemSelected(item);
         }
 
         protected override void OnPause()
         {
+            subscription.Dispose();
             base.OnPause();
         }
     }
--- FacebookPagesAppModels/PagesModel.cs
+++ FacebookPagesAppModels/PagesModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Reactive;
+using System.Reactive.Linq;
 using System.Windows.Input;
 using ReactiveUI;
 using RxApp;
@@ -12,6 +13,9 @@ namespace FacebookPagesApp
         IReadOnlyReactiveList<object> Posts { get; }
 
         ICommand CreatePost { get; }
+
+        ICommand RefeshPosts { get; }
+        bool RefreshingPosts { get; }
     }
 
     public interface IPagesControllerModel : INavigableControllerModel, IServiceControllerModel
@@ -20,61 +24,48 @@ namespace FacebookPagesApp
         IReactiveList<object> Posts { get; }
 
         IObservable<Unit> CreatePost { get; }
+
+        IObservable<Unit> RefreshPosts { get; }
+        bool RefreshingPosts { set; }
     }
 
     public class PagesModel : MobileModel, IPagesViewModel, IPagesControllerModel
     {
+        private readonly ReactiveList<object> _pages = new ReactiveList<object>();
+        private readonly ReactiveList<object> _posts = new ReactiveList<object>();
+        private readonly IReactiveCommand<object> _createPost = ReactiveCommand.Create();
+        private readonly IReactiveCommand<object> _refreshPosts;
+
+        private bool _refreshingPosts = false;
+
         public PagesModel()
         {
+            _refreshPosts = this.WhenAnyValue(x => ((IPagesViewModel) x).RefreshingPosts).Select(x => !x).ToCommand();
         }
 
-        IReadOnlyReactiveList<object> IPagesViewModel.Pages
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
+        IReadOnlyReactiveList<object> IPagesViewModel.Pages { get { return _pages; } }
 
-        IReadOnlyReactiveList<object> IPagesViewModel.Posts
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
+        IReactiveList<object> IPagesControllerModel.Pages { get { return _pages; } }
 
-        ICommand IPagesViewModel.CreatePost
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
 
-        IReactiveList<object> IPagesControllerModel.Pages
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
+        IReadOnlyReactiveList<object> IPagesViewModel.Posts { get { return _posts; } }
 
-        IReactiveList<object> IPagesControllerModel.Posts
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
+        IReactiveList<object> IPagesControllerModel.Posts { get { return _posts; } }
 
-        IObservable<Unit> IPagesControllerModel.CreatePost
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
+
+        ICommand IPagesViewModel.CreatePost { get { return _createPost; } }
+
+        IObservable<Unit> IPagesControllerModel.CreatePost { get { return _createPost.Select(_ => Unit.Default); } }
+
+
+        ICommand IPagesViewModel.RefeshPosts { get { return _refreshPosts; } }
+
+        IObservable<Unit> IPagesControllerModel.RefreshPosts { get { return _refreshPosts.Select(_ => Unit.Default); } }
+
+
+        bool IPagesViewModel.RefreshingPosts { get { return _refreshingPosts; } }
+
+        bool IPagesControllerModel.RefreshingPosts { set { this.RaiseAndSetIfChanged(ref _refreshingPosts, value); } }
     }
 }
 

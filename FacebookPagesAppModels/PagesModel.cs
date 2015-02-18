c3f37f4bdab2cece69d55ce2b8f5a88fa7dc20a2
Dave <bordoley@gmail.com>
2015-02-17 21:40:06 -0800

    Import persistent vector from FSharpx.Collections and use it for the view model representation of the posts list.

--- FacebookPagesAppModels/PagesModel.cs
+++ FacebookPagesAppModels/PagesModel.cs
@@ -4,6 +4,7 @@ using System.Reactive.Linq;
 using RxApp;
 using Splat;
 using Microsoft.FSharp.Core;
+using FSharpx.Collections;
 
 using Unit = System.Reactive.Unit;
 
@@ -19,13 +20,16 @@ namespace FacebookPagesApp
 
         IObservable<IEnumerable<FacebookAPI.Page>> Pages { get; }
 
-        //IRxReadOnlyList<object> Posts { get; }
-
         IRxCommand CreatePost { get; }
 
         IRxCommand LogOut { get; }
 
+
+        IObservable<IReadOnlyList<FacebookAPI.Post>> Posts { get; }
+
         IRxCommand RefeshPosts { get; }
+
+        IRxCommand LoadMorePosts { get; }
     }
 
     public interface IPagesControllerModel : INavigableControllerModel, IServiceControllerModel
@@ -42,28 +46,40 @@ namespace FacebookPagesApp
 
         IObservable<FacebookAPI.Page> LoadPage { get; }
 
-        //IRxList<object> Posts { get; }
-
         IObservable<Unit> CreatePost { get; }
 
         IObservable<Unit> LogOut { get; }
 
+
+        IRxProperty<PersistentVector<FacebookAPI.Post>> Posts { get; }
+       
+        bool CanLoadMorePosts { set; }
+        IObservable<Unit> LoadMorePosts { get; }
+
+        bool CanRefreshPosts { set; }
         IObservable<Unit> RefreshPosts { get; }
-        bool RefreshingPosts { set; }
     }
 
     public class PagesModel : MobileModel, IPagesViewModel, IPagesControllerModel
     { 
-        //private readonly IRxList<object> _posts = RxList.Create<object>();
+        private readonly IRxProperty<PersistentVector<FacebookAPI.Post>> _posts = 
+            RxProperty.Create(PersistentVector<FacebookAPI.Post>.Empty());
+
+        private readonly IRxCommand _loadMorePosts;
+        private readonly IRxProperty<bool> _canLoadMorePosts = RxProperty.Create(false);
+
+        private readonly IRxCommand _refreshPosts;
+        private readonly IRxProperty<bool> _canRefreshPosts = RxProperty.Create(false);
+
 
         private readonly IRxCommand _createPost = RxCommand.Create();
         private readonly IRxCommand _logOut = RxCommand.Create();
-        private readonly IRxCommand _refreshPosts;
+
 
         private readonly IRxProperty<IEnumerable<FacebookAPI.Page>> _pages = 
             RxProperty.Create((IEnumerable<FacebookAPI.Page>) new List<FacebookAPI.Page>());
 
-        private readonly IRxProperty<bool> _refreshingPosts = RxProperty.Create(false);
+
         private readonly IRxProperty<bool> _showUnpublishedPosts =  RxProperty.Create<bool>(false);
         private readonly IRxProperty<string> _userName = RxProperty.Create<string>("");
         private readonly IRxProperty<IBitmap> _profilePhoto = RxProperty.Create<IBitmap>(null);
@@ -72,11 +88,9 @@ namespace FacebookPagesApp
 
         public PagesModel()
         {
-            _refreshPosts = _refreshingPosts.ToCommand();
+            _refreshPosts = _canRefreshPosts.ToCommand();
+            _loadMorePosts = _canLoadMorePosts.ToCommand();
         }
-
-        bool IPagesControllerModel.RefreshingPosts { set { _refreshingPosts.Value = value; } }
-
 
         IRxProperty<bool> IPagesViewModel.ShowUnpublishedPosts { get { return _showUnpublishedPosts; } }
 
@@ -111,11 +125,6 @@ namespace FacebookPagesApp
         IRxProperty<IEnumerable<FacebookAPI.Page>> IPagesControllerModel.Pages { get { return _pages; } }
 
 
-        //IRxReadOnlyList<object> IPagesViewModel.Posts { get { return _posts.ToRxReadOnlyList(); } }
-
-        //IRxList<object> IPagesControllerModel.Posts { get { return _posts; } }
-
-
         IRxCommand IPagesViewModel.CreatePost { get { return _createPost; } }
 
         IObservable<Unit> IPagesControllerModel.CreatePost { get { return _createPost; } }
@@ -126,9 +135,30 @@ namespace FacebookPagesApp
         IObservable<Unit> IPagesControllerModel.LogOut { get { return _logOut; } }
 
 
+
         IRxCommand IPagesViewModel.RefeshPosts { get { return _refreshPosts; } }
 
         IObservable<Unit> IPagesControllerModel.RefreshPosts { get { return _refreshPosts; } }
+
+        bool IPagesControllerModel.CanRefreshPosts { set { _canRefreshPosts.Value = value; } }
+
+
+        IRxCommand IPagesViewModel.LoadMorePosts { get { return _loadMorePosts; } }
+
+        IObservable<Unit> IPagesControllerModel.LoadMorePosts { get { return _loadMorePosts; } }
+
+        bool IPagesControllerModel.CanLoadMorePosts { set { _canLoadMorePosts.Value = value; } }
+
+
+        IObservable<IReadOnlyList<FacebookAPI.Post>> IPagesViewModel.Posts
+        {
+            get { return this._posts.Select(x => (IReadOnlyList<FacebookAPI.Post>)x); }
+        }
+
+        IRxProperty<PersistentVector<FacebookAPI.Post>> IPagesControllerModel.Posts
+        {
+            get { return this._posts; }
+        }
     }
 }
 
